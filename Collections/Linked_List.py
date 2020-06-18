1c71ac6f983cc0a6218f37e2d27f6f85cdccc906
Ammar Alam Malik <32976151+ammalik221@users.noreply.github.com>
2020-06-18 23:16:14 +0530

    Update Linked_List.py

--- Collections/Linked_List.py
+++ Collections/Linked_List.py
@@ -1,2 +1,10 @@
 if __name__ == "__main__":
     #test cases
+    a = Element(10)
+    b = Element(22)
+    
+    linked_list = LinkedList(a)
+    linked_list.append(a)
+    linked_list.append(b)
+    
+    linked_list.print_list()
