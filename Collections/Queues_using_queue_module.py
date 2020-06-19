0c84a2182194cb43c9af34481b072044778ffd17
Ammar Alam Malik <32976151+ammalik221@users.noreply.github.com>
2020-06-19 21:29:18 +0530

    Update Queues_using_queue_module.py

--- Collections/Queues_using_queue_module.py
+++ Collections/Queues_using_queue_module.py
@@ -1,7 +1,24 @@
 """
-Queues Implementation in Python 3.0 using queue module.
+Queues Implementation in Python 3.0 using deque module.
 
 For implementations from scratch, check out the other files in this repository.
 """
 
-from collections import queue
+from collections import deque
+
+# test cases
+# make a deque
+q = deque()
+
+# add elements
+q.append(20)
+q.append(30)
+q.append(40)
+
+# output is - 10 20 30 40
+print(q)
+
+# remove elements from queue
+# output is 10 and 20 respectively
+print(q.popleft())
+print(q.popleft())
