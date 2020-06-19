bbaf0556ec53fd107e63c34cc7a1f81718ca933c
Ammar Alam Malik <32976151+ammalik221@users.noreply.github.com>
2020-06-19 22:28:25 +0530

    Update and rename Binary_Search to Binary_Search.py

---
+++ Searching_and_Sorting/Binary_Search.py
@@ -0,0 +1,8 @@
+"""
+Binary Search Implementation in Python 3.0
+
+This file has binary search implemented using loops.
+Check out Binary_search_recursion.py for the implementation using recursion.
+"""
+
+
