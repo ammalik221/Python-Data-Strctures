b2b5feb52504dc26dad1be2ea88a90cbe52b32ea
Ammar Alam Malik <32976151+ammalik221@users.noreply.github.com>
2020-06-20 00:57:59 +0530

    Update Fibonacci_numbers.py

--- Searching_and_Sorting/Fibonacci_numbers.py
+++ Searching_and_Sorting/Fibonacci_numbers.py
@@ -2,3 +2,16 @@
 Example of recursion in Python 3.0
 """
 
+def fibonacci(pos):
+    if pos < 0:
+        print("Enter valid number (>=0)")
+    elif pos == 0 or pos == 1:
+        return pos
+    else:
+        return fibonacci(pos-1) + fibonacci(pos-2)
+    return -1
+  
+if __name__ == "__main__":
+    print(fibonacci(9))
+    print(fibonacci(1))
+    print(fibonacci(27))
