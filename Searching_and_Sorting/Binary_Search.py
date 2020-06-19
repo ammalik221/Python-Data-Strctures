98ac2eae4ba7770db9d1fde7f3a6c7800b562228
Ammar Alam Malik <32976151+ammalik221@users.noreply.github.com>
2020-06-19 22:47:16 +0530

    Update Binary_Search.py

--- Searching_and_Sorting/Binary_Search.py
+++ Searching_and_Sorting/Binary_Search.py
@@ -6,3 +6,33 @@ Check out Binary_search_recursion.py for the implementation using recursion.
 """
 
 
+def binary_search(array, value):
+    """ searching for the element starting from the middle """
+    lower = 0
+    higher = len(array)
+    while(higher >= lower):
+        # keep calculating middle value again and again according to updated lower and higher value
+        mid = (lower + higher)//2
+        if array[mid] == value:
+            # value found
+            return mid
+        elif array[mid] > value:
+            # search the left side of array
+            higher = mid - 1
+        else:
+            # search the right side of array
+            lower = mid + 1
+    return -1
+  
+if __name__ == "__main__":
+    # test case
+    a = [4, 5, 6, 10, 13, 14, 16, 18, 19, 25, 45, 67]
+    value_1 = 10
+    value_2 = 23
+    value_3 = 67
+    
+    # outputs will be 3, -1 and 11 respectively
+    print(binary_search(a, value_1))
+    print(binary_search(a, value_2))
+    print(binary_search(a, value_3))
+    
