7a7a9f110c17cfbc9f44ea1b0bf878e59dd6a736
Ammar Alam Malik <32976151+ammalik221@users.noreply.github.com>
2020-06-20 13:14:31 +0530

    Update Priority_queues.py

--- Collections/Priority_queues.py
+++ Collections/Priority_queues.py
@@ -1,4 +1,9 @@
 """
 Priority Queues Implementation in Python 3.0.
+
+This is Max Priority Queue, which arranges the elements in descending order,
+i.e. the element with the highest priority is dequed first.
 """
+
+
 
