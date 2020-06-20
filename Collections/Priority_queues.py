f08f157ed75bb4494b1e726e30e124a4844451c6
Ammar Alam Malik <32976151+ammalik221@users.noreply.github.com>
2020-06-20 12:49:27 +0530

    Update Priority_queues.py

--- Collections/Priority_queues.py
+++ Collections/Priority_queues.py
@@ -1,6 +1,4 @@
 """
-Stacks Implementation in Python 3.0.
-
-Although Python already provides stack functionality using .pop() and .append() function, 
-but it's always nice to see how things work under the hood by implementing them on your own :)
+Priority Queues Implementation in Python 3.0.
 """
+
