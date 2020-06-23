90d32a978273aa26931f4c11945f668dbd56f76f
Ammar Alam Malik <32976151+ammalik221@users.noreply.github.com>
2020-06-23 15:05:05 +0530

    Create Binary_search_tree.py

---
+++ Trees/Binary_search_tree.py
@@ -0,0 +1,10 @@
+"""
+Binary Search Tree implementation in Python 3.0.
+
+Main difference between a normal tree and a binary search tree is the ordering of elements.
+In binary search tree, the parent node is greater than the left child and smaller than the right child.
+
+By using this condition, the runtime of operations like inserting decreases from O(n) to O(logn)
+"""
+
+class Node(ob
